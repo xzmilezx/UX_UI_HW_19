1b86128a1ce3939bf9c08635cd364c3200e6ab0a
xzmilezx <xzmilezx@gmail.com>
2022-08-05 00:03:22 -0400

    homeAnimation

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+function moveImg() {
+    $('.pham').animate({left: '125px', top: '-660px'}, 2000);
+    $('.phamImg').animate({height:'185px'}, 2000);
+    $('.tu').animate({left: '-270px', top:'-650px'}, 2000);
+    $('.tuImg').animate({height:'120px'}, 2000);
+    $('.tuProfile').animate({width: '330px'}, 2000);
+    $('.tuPic').animate({top: '-550px',left:'10%'}, 2000);
+    $('.adjText').animate({left: '125px', top:'-830px'}, 2000);
+
+}
+
+// $( document ).ready( moveImg );
